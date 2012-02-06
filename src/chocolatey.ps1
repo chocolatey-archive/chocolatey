f78ebffb676a88bf8867294ac9fe2cfc5b5ee216
Rob Reynolds <ferventcoder@gmail.com>
2012-02-06 07:52:55 -0600

    Ignoring files that have a .ignore file sitting next to them with exact casing...

--- src/chocolatey.ps1
+++ src/chocolatey.ps1
@@ -279,15 +279,24 @@ Looking for executables in folder: $packageFolder
 Adding batch files for any executables found to a location on PATH. In other words the executable will be available from ANY command line/powershell prompt.
 $h2
 "@ | Write-Host
+    $batchCreated = $false
     try {
       $files = get-childitem $packageFolder -include *.exe -recurse
       foreach ($file in $files) {
-        Generate-BinFile $file.Name.Replace(".exe","").Replace(".EXE","") $file.FullName
+        if (!(test-path($file.FullName + '.ignore'))) {
+          Generate-BinFile $file.Name.Replace(".exe","").Replace(".EXE","") $file.FullName
+          $batchCreated = $true
+        }
       }
     }
     catch {
-      Write-Host 'There are no executables in the package.'
+      #Write-Host 'There are no executables (that are not ignored) in the package.'
     }
+    
+    if (!($batchCreated)) {
+      Write-Host 'There are no executables (that are not ignored) in the package.'
+    }
+    
     Write-Host "$h2"
   }
 }
