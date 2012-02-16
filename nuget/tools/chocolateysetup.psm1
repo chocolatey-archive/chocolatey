6d56726e9aca6818d3fc9810d943d16eff6a4c14
Rob Reynolds <ferventcoder@gmail.com>
2012-02-16 01:21:44 -0600

    Moving the default installation of chocolatey to C:\Chocolatey instead of C:\NuGet - https://github.com/chocolatey/chocolatey/issues/58

--- nuget/tools/chocolateysetup.psm1
+++ nuget/tools/chocolateysetup.psm1
@@ -1,7 +1,8 @@
 $thisScriptFolder = (Split-Path -parent $MyInvocation.MyCommand.Definition)
 $chocInstallVariableName = "ChocolateyInstall"
 $sysDrive = $env:SystemDrive
-$defaultNugetPath = "$sysDrive\NuGet"
+#$defaultNugetPath = "$sysDrive\Chocolatey"
+$defaultChocolateyPathOld = "$sysDrive\NuGet"
 
 function Set-ChocolateyInstallFolder($folder){
   if(test-path $folder){
@@ -22,18 +23,21 @@ function Create-DirectoryIfNotExists($folderName){
 }
 
 function Create-ChocolateyBinFiles {
-param([string] $nugetChocolateyPath,[string] $nugetExePath)
+param(
+  [string] $nugetChocolateyPath,
+  [string] $chocolateyExePath
+)
 
-$nugetChocolateyBinFile = Join-Path $nugetExePath 'chocolatey.bat'
-$nugetChocolateyInstallAlias = Join-Path $nugetExePath 'cinst.bat'
-$nugetChocolateyInstallIfMissingAlias = Join-Path $nugetExePath 'cinstm.bat'
-$nugetChocolateyUpdateAlias = Join-Path $nugetExePath 'cup.bat'
-$nugetChocolateyListAlias = Join-Path $nugetExePath 'clist.bat'
-$nugetChocolateyVersionAlias = Join-Path $nugetExePath 'cver.bat'
-$nugetChocolateyWebPiAlias = Join-Path $nugetExePath 'cwebpi.bat'
-$nugetChocolateyGemAlias = Join-Path $nugetExePath 'cgem.bat'
-$nugetChocolateyPackAlias = Join-Path $nugetExePath 'cpack.bat'
-$nugetChocolateyPushAlias = Join-Path $nugetExePath 'cpush.bat'
+$nugetChocolateyBinFile = Join-Path $chocolateyExePath 'chocolatey.bat'
+$nugetChocolateyInstallAlias = Join-Path $chocolateyExePath 'cinst.bat'
+$nugetChocolateyInstallIfMissingAlias = Join-Path $chocolateyExePath 'cinstm.bat'
+$nugetChocolateyUpdateAlias = Join-Path $chocolateyExePath 'cup.bat'
+$nugetChocolateyListAlias = Join-Path $chocolateyExePath 'clist.bat'
+$nugetChocolateyVersionAlias = Join-Path $chocolateyExePath 'cver.bat'
+$nugetChocolateyWebPiAlias = Join-Path $chocolateyExePath 'cwebpi.bat'
+$nugetChocolateyGemAlias = Join-Path $chocolateyExePath 'cgem.bat'
+$nugetChocolateyPackAlias = Join-Path $chocolateyExePath 'cpack.bat'
+$nugetChocolateyPushAlias = Join-Path $chocolateyExePath 'cpush.bat'
 
 Write-Host "Creating `'$nugetChocolateyBinFile`' so you can call 'chocolatey' from anywhere."
 "@echo off
@@ -85,12 +89,12 @@ function Initialize-Chocolatey {
 			b) Installs chocolatey into the "nugetPath"
 			c) Adds chocolaty to the PATH environment variable so you have access to the chocolatey|cinst commands.
 	.PARAMETER  NuGetPath
-		Allows you to override the default path of (C:\NuGet\) by specifying a directory chocolaty will install nuget packages.
+		Allows you to override the default path of (C:\Chocolatey\) by specifying a directory chocolaty will install nuget packages.
 
 	.EXAMPLE
 		C:\PS> Initialize-Chocolatey
 
-		Installs chocolatey into the default C:\NuGet\ directory.
+		Installs chocolatey into the default C:\Chocolatey\ directory.
 
 	.EXAMPLE
 		C:\PS> Initialize-Chocolatey -nugetPath "D:\ChocolateyInstalledNuGets\"
@@ -99,51 +103,52 @@ function Initialize-Chocolatey {
 
 #>
 param(
-  [Parameter(Mandatory=$false)]
-  [string]$nugetPath = "$sysDrive\NuGet"
+  [Parameter(Mandatory=$false)][string]$chocolateyPath = "$sysDrive\Chocolatey"
 )
 
-  if(!(test-path $nugetPath)){
-    mkdir $nugetPath | out-null
+  if(!(test-path $chocolateyPath)){
+    mkdir $chocolateyPath | out-null
   }
   
-	#if we have an already environment variable path, use it.
-	$alreadyInitializedNugetPath = Get-ChocolateyInstallFolder
-  if($alreadyInitializedNugetPath -and $alreadyInitializedNugetPath -ne $nugetPath){
-    $nugetPath = $alreadyInitializedNugetPath
+  #if we have an already environment variable path, use it.
+  $alreadyInitializedNugetPath = Get-ChocolateyInstallFolder
+  if($alreadyInitializedNugetPath -and $alreadyInitializedNugetPath -ne $chocolateyPath -and $alreadyInitializedNugetPath -ne $defaultChocolateyPathOld){
+    $chocolateyPath = $alreadyInitializedNugetPath
   }
   else {
-    Set-ChocolateyInstallFolder $nugetPath
+    Set-ChocolateyInstallFolder $chocolateyPath
   }
 
   #set up variables to add
-  $nugetExePath = Join-Path $nuGetPath 'bin'
-  $nugetLibPath = Join-Path $nuGetPath 'lib'
-  $nugetChocolateyPath = Join-Path $nuGetPath 'chocolateyInstall'
+  $chocolateyExePath = Join-Path $chocolateyPath 'bin'
+  $chocolateyLibPath = Join-Path $chocolateyPath 'lib'
+  $nugetChocolateyPath = Join-Path $chocolateyPath 'chocolateyInstall'
 
-  $nugetYourPkgPath = [System.IO.Path]::Combine($nugetLibPath,"yourPackageName")
+  $nugetYourPkgPath = [System.IO.Path]::Combine($chocolateyLibPath,"yourPackageName")
 @"
 We are setting up the Chocolatey repository for NuGet packages that should be at the machine level. Think executables/application packages, not library packages.
 That is what Chocolatey NuGet goodness is for.
-The repository is set up at `'$nugetPath`'.
-The packages themselves go to `'$nugetLibPath`' (i.e. $nugetYourPkgPath).
-A batch file for the command line goes to `'$nugetExePath`' and points to an executable in `'$nugetYourPkgPath`'.
+The repository is set up at `'$chocolateyPath`'.
+The packages themselves go to `'$chocolateyLibPath`' (i.e. $nugetYourPkgPath).
+A batch file for the command line goes to `'$chocolateyExePath`' and points to an executable in `'$nugetYourPkgPath`'.
 
 Creating Chocolatey NuGet folders if they do not already exist.
 
 "@ | Write-Host
 
   #create the base structure if it doesn't exist
-  Create-DirectoryIfNotExists $nugetExePath
-  Create-DirectoryIfNotExists $nugetLibPath
+  Create-DirectoryIfNotExists $chocolateyExePath
+  Create-DirectoryIfNotExists $chocolateyLibPath
   Create-DirectoryIfNotExists $nugetChocolateyPath
   
-  Install-ChocolateyFiles $nugetPath
+  Upgrade-OldNuGetDirectory $defaultChocolateyPathOld $chocolateyPath
   
-  $nugetExePathVariable = $nugetExePath.ToLower().Replace($nugetPath.ToLower(), "%DIR%..\").Replace("\\","\")
-  Create-ChocolateyBinFiles $nugetChocolateyPath.ToLower().Replace($nugetPath.ToLower(), "%$($chocInstallVariableName)%\").Replace("\\","\") $nugetExePath
-  Initialize-ChocolateyPath $nugetExePath $nugetExePathVariable
-  Process-ChocolateyBinFiles $nugetExePath $nugetExePathVariable
+  Install-ChocolateyFiles $chocolateyPath
+  
+  $chocolateyExePathVariable = $chocolateyExePath.ToLower().Replace($chocolateyPath.ToLower(), "%DIR%..\").Replace("\\","\")
+  Create-ChocolateyBinFiles $nugetChocolateyPath.ToLower().Replace($chocolateyPath.ToLower(), "%$($chocInstallVariableName)%\").Replace("\\","\") $chocolateyExePath
+  Initialize-ChocolateyPath $chocolateyExePath $chocolateyExePathVariable
+  Process-ChocolateyBinFiles $chocolateyExePath $chocolateyExePathVariable
   
 @"
 Chocolatey is now ready.
@@ -153,63 +158,96 @@ You may need to shut down and restart powershell and/or consoles first prior to 
 "@ | write-host
 }
 
+function Upgrade-OldNuGetDirectory {
+param(
+  [string]$chocolateyPathOld = "$sysDrive\NuGet",
+  [string]$chocolateyPath = "$sysDrive\NuGet"
+)
+
+  if((test-path $defaultChocolateyPathOld)){
+    Write-Host "Upgrading `'$chocolateyPathOld`' to `'$chocolateyPath`'."
+    
+    Write-Host "Copying the contents of `'$chocolateyPathOld`' to `'$chocolateyPath`'. This step may fail if you have anything in this folder running or locked."
+    Write-Host 'If it fails, just manually copy the rest of the items out and then delete the folder.'
+    Copy-Item "$($chocolateyPathOld)\*" "$chocolateyPath" -force -recurse
+    write-host "Attempting to remove `'$chocolateyPathOld`'. This may fail if something in the folder is being used or locked. If it fails, same idea as above."
+    Remove-Item "$($chocolateyPathOld)" -force -recurse
+    
+    $chocolateyExePathOld = Join-Path $chocolateyPathOld 'bin'
+    $statementTerminator = ";"
+    #get the PATH variable
+    $envPath = $env:PATH
+    #remove the old environment variable
+    if ($envPath.Contains($chocolateyExePathOld)) {
+      Write-Host "Attempting to remove older `'$chocolateyExePathOld`' from the PATH."
+      $userPath = [Environment]::GetEnvironmentVariable('Path', [System.EnvironmentVariableTarget]::User)
+      if ($userPath.Contains($chocolateyExePathOld)) {
+        $userPath = $userPath.Replace("$chocolateyExePathOld","").Replace("$chocolateyExePathOld);","")
+        [Environment]::SetEnvironmentVariable('Path', $userPath, [System.EnvironmentVariableTarget]::User)
+      } else {
+        Write-Host "Chocolatey was not able to remove `'$chocolateyExePathOld`' automatically. It's likely in the system's path instead of the user path, but it could be due to other factors (including casing). Please manually find and remove it from either the user PATH or the machine PATH."
+      }
+    }
+  }
+}
+
 function Install-ChocolateyFiles {
 param(
-  [string]$nugetPath = "$sysDrive\NuGet"
+  [string]$chocolateyPath = "$sysDrive\Chocolatey"
 )
   #$chocInstallFolder = Get-ChildItem .\ -Recurse | ?{$_.name -match  "chocolateyInstall*"} | sort name -Descending | select -First 1 
   #$thisScript = (Get-Variable MyInvocation -Scope 1).Value 
   #$thisScriptFolder = Split-Path $thisScript.MyCommand.Path
   
   $chocInstallFolder = Join-Path $thisScriptFolder "chocolateyInstall"
-  Write-Host "Copying the contents of `'$chocInstallFolder`' to `'$nugetPath`'."
-  Copy-Item $chocInstallFolder $nugetPath -recurse -force
+  Write-Host "Copying the contents of `'$chocInstallFolder`' to `'$chocolateyPath`'."
+  Copy-Item $chocInstallFolder $chocolateyPath -recurse -force
 }
 
 function Initialize-ChocolateyPath {
 param(
-  [string]$nugetExePath = "$sysDrive\NuGet\bin"
-  ,[string]$nugetExePathVariable = "%$($chocInstallVariableName)%\bin"
+  [string]$chocolateyExePath = "$sysDrive\Chocolatey\bin",
+  [string]$chocolateyExePathVariable = "%$($chocInstallVariableName)%\bin"
 )
 
   $statementTerminator = ";"
   #get the PATH variable
   $envPath = $env:PATH
   
-  #if you do not find $nugetPath\bin, add it 
-  if (!$envPath.ToLower().Contains($nugetExePath.ToLower())) # -and !$envPath.ToLower().Contains($nugetExePathVariable))
+  #if you do not find $chocolateyPath\bin, add it 
+  if (!$envPath.ToLower().Contains($chocolateyExePath.ToLower())) # -and !$envPath.ToLower().Contains($chocolateyExePathVariable))
   {
     Write-Host ''
     #now we update the path
-    Write-Host 'PATH environment variable does not have ' $nugetExePath ' in it. Adding.'
-    #Write-Host 'PATH environment variable does not have ' $nugetExePathVariable ' in it. Adding.'
+    Write-Host 'PATH environment variable does not have ' $chocolateyExePath ' in it. Adding.'
+    #Write-Host 'PATH environment variable does not have ' $chocolateyExePathVariable ' in it. Adding.'
     $userPath = [Environment]::GetEnvironmentVariable('Path', [System.EnvironmentVariableTarget]::User)
   
     #does the path end in ';'?
     $hasStatementTerminator = $userPath -ne $null -and $userPath.EndsWith($statementTerminator)
     # if the last digit is not ;, then we are adding it
-    If (!$hasStatementTerminator -and $userPath -ne $null) {$nugetExePath = $statementTerminator + $nugetExePath}
-    $userPath = $userPath + $nugetExePath + $statementTerminator
+    If (!$hasStatementTerminator -and $userPath -ne $null) {$chocolateyExePath = $statementTerminator + $chocolateyExePath}
+    $userPath = $userPath + $chocolateyExePath + $statementTerminator
 
     [Environment]::SetEnvironmentVariable('Path', $userPath, [System.EnvironmentVariableTarget]::User)
 
     #add it to the local path as well so users will be off and running
     $envPSPath = $env:PATH
-    $env:Path = $envPSPath + $statementTerminator + $nugetExePath + $statementTerminator
-    $env:ChocolateyInstall = $nugetExePath
+    $env:Path = $envPSPath + $statementTerminator + $chocolateyExePath + $statementTerminator
+    $env:ChocolateyInstall = $chocolateyExePath
   } else {
-    write-host "User PATH already contains either `'$nugetExePath`' or `'$nugetExePathVariable`'"
+    write-host "User PATH already contains either `'$chocolateyExePath`' or `'$chocolateyExePathVariable`'"
   }
 }
 
 function Process-ChocolateyBinFiles {
 param(
-  [string]$nugetExePath = "$($env:SystemDrive)\NuGet\bin"
-  ,[string]$nugetExePathVariable = "%$($chocInstallVariableName)%\bin"
+  [string]$chocolateyExePath = "$($env:SystemDrive)\Chocolatey\bin",
+  [string]$chocolateyExePathVariable = "%$($chocInstallVariableName)%\bin"
 )
-  $processedMarkerFile = Join-Path $nugetExePath '_processed.txt'
+  $processedMarkerFile = Join-Path $chocolateyExePath '_processed.txt'
   if (!(test-path $processedMarkerFile)) {
-    $files = get-childitem $nugetExePath -include *.bat -recurse
+    $files = get-childitem $chocolateyExePath -include *.bat -recurse
     foreach ($file in $files) {
       Write-Host "Processing $($file.Name) to make it portable"
       $fileStream = [System.IO.File]::Open("$file", 'Open', 'Read', 'ReadWrite')
@@ -218,7 +256,7 @@ param(
       $reader.Close()
       $fileStream.Close()
       
-      $fileText = $fileText.ToLower().Replace("`"" + $nugetPath.ToLower(), "SET DIR=%~dp0%`n""%DIR%..\").Replace("\\","\")
+      $fileText = $fileText.ToLower().Replace("`"" + $chocolateyPath.ToLower(), "SET DIR=%~dp0%`n""%DIR%..\").Replace("\\","\")
       
       Set-Content $file -Value $fileText -Encoding Ascii
     }
