4a97f67da24532d2c18d90fcb2687ffc925ff2d0
Rob Reynolds <ferventcoder@gmail.com>
2012-05-15 00:45:00 -0500

    Long Package Version returns prerelease package part as part of the long version

--- src/functions/Get-LongPackageVersion.ps1
+++ src/functions/Get-LongPackageVersion.ps1
@@ -4,5 +4,13 @@ param(
 )
   #todo - make this compare prerelease information as well
   $longVersion = $packageVersion.Split('-')[0].Split('.') | %{('0' * (8 - $_.Length)) + $_}
-  return [System.String]::Join('.',$longVersion)
+  
+  $longVersionReturn = [System.String]::Join('.',$longVersion)
+  
+  if ($packageVersion.Contains('-')) {
+    $prerelease = $packageVersion.Substring($packageVersion.IndexOf('-') + 1)
+    $longVersionReturn += ".$($prerelease)"
+  }
+  
+  return $longVersionReturn
 }
