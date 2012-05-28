e67d21b80e8d049a685c619b345007e8b34446e1
Rich Siegel <rsiegel@ise.com>
2012-05-28 00:01:11 -0400

    initial draft for chocolatey uninstall function

---
+++ src/functions/Chocolatey-Uninstall.ps1
@@ -0,0 +1,34 @@
+﻿function Chocolatey-Uninstall { 
+param(
+  [string] $packageName,
+  [string] $version = '',
+  [string] $installerArguments = ''
+)
+
+  if ($packageName -eq 'all') { 
+    write-host "Uninstalling all packages is not yet supported in this version. "  
+	# by default this should prompt user 2x.  Also can provide a -nuke switch for prompt bypass
+    return
+  }
+
+@"
+$h1
+Chocolatey ($chocVer) is uninstalling $packageName from "$nugetLibPath". 
+$h1
+"@ | Write-Host
+	
+	$packages = $packageName
+	foreach ($package in $packages) {
+		$versions = Chocolatey-Version $package $source
+		if ($versions.found -eq "no version") {
+			write-host "not installed"
+		}
+		else {
+		#this is where the meat and the potatos goes
+		write-host "installed"
+
+		$packageFolder = Join-Path $nugetLibPath "$($package).$($versions.found)" 
+        Run-ChocolateyPS1 $packageFolder $package "uninstall"	
+		}
+	}
+}
