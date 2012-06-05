e43408ec5f336009fb224194672214f72e14f23f
Rob Reynolds <ferventcoder@gmail.com>
2012-06-05 10:41:29 -0500

    cygwin improvements to look like the others

--- src/functions/Chocolatey-Cygwin.ps1
+++ src/functions/Chocolatey-Cygwin.ps1
@@ -15,23 +15,20 @@ $h1
   $chocoInstallLog = Join-Path $nugetChocolateyPath 'chocolateyCygwinInstall.log';
   Remove-LastInstallLog $chocoInstallLog
  
-  $cygwinArgs = "/c cyg-get $packageName"
-#  if ($installerArguments -ne '') {
-#    $cygwinArgs = "$cygwinArgs $installerArguments"
-#  }
-#  if ($overrideArgs -eq $true) {
-#    $cygwinArgs = "/c cyg-get $installerArguments /Products:$packageName"
-#    write-host "Overriding arguments for cyg-get"
-#  }  
+  $packageArgs = "/c cyg-get $packageName"
+  # if ($version -notlike '') {
+  #   $packageArgs = "$packageArgs -v $version";
+  # }
   
-  Write-Host "Opening minimized PowerShell window and calling `'cmd.exe $cygwinArgs`'. If progress is taking a long time, please check that window. It also may not be 100% silent..."
+  if ($installerArguments -ne '') {
+    $packageArgs = "$packageArgs $installerArguments";
+  }
+
+  Write-Host "Opening minimized PowerShell window and calling `'cmd.exe $packageArgs`'. If progress is taking a long time, please check that window. It also may not be 100% silent..."
+  Start-Process -FilePath "$($env:windir)\System32\WindowsPowerShell\v1.0\powershell.exe" -ArgumentList "-NoProfile -ExecutionPolicy unrestricted -Command `"cmd.exe $packageArgs | Tee-Object -FilePath $chocoInstallLog`"" -Verb "RunAs"  -Wait -WindowStyle Minimized
   
-  #Start-Process -FilePath "cmd" -ArgumentList "$cygwinArgs" -Verb "runas"  -Wait >$chocoInstallLog #-PassThru -UseNewEnvironment >
-  #Start-Process -FilePath "$($env:windir)\System32\WindowsPowerShell\v1.0\powershell.exe" -ArgumentList "-NoProfile -ExecutionPolicy unrestricted -Command `"cmd.exe $cygwinArgs | Out-String`"" -Verb "runas"  -Wait | Write-Host  #-PassThru -UseNewEnvironment
-  Start-Process -FilePath "$($env:windir)\System32\WindowsPowerShell\v1.0\powershell.exe" -ArgumentList "-NoProfile -ExecutionPolicy unrestricted -Command `"cmd.exe $cygwinArgs | Tee-Object -FilePath $chocoInstallLog`"" -Verb "RunAs"  -Wait -WindowStyle Minimized
-  
-  $cygwinOutput = Get-Content $chocoInstallLog -Encoding Ascii
-	foreach ($line in $cygwinOutput) {
+  $installOutput = Get-Content $chocoInstallLog -Encoding Ascii
+  foreach ($line in $installOutput) {
     Write-Host $line
   }
   
