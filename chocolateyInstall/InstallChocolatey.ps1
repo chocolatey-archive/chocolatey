c2db990610f71ce748bd2a9e3764fdf3d29c5bbe
Rob Reynolds <ferventcoder@gmail.com>
2012-06-04 20:45:37 -0500

    setting back to the most up to date version of choco

--- chocolateyInstall/InstallChocolatey.ps1
+++ chocolateyInstall/InstallChocolatey.ps1
@@ -18,7 +18,7 @@
 
 # variables
 #$url = "http://chocolatey.org/packages/chocolatey/DownloadPackage"
-$url = "http://chocolatey.org/api/v2/package/chocolatey/0.9.8.16"
+$url = "http://chocolatey.org/api/v2/package/chocolatey/"
 #$url = "http://chocolatey.org/api/v1/package/chocolatey"
 $chocTempDir = Join-Path $env:TEMP "chocolatey"
 $tempDir = Join-Path $chocTempDir "chocInstall"
