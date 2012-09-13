eb222d5381ed36fbfa12a065956fff498bba79cd
Michael Lambert <mlambert125@live.com>
2012-09-13 10:41:27 -0400

    Fixed issue with nuget stdout handling
    Using the Start-Process cmdlet with -RedirectStandardOutput does not
    reliably flush output to the file immediately.  This causes the text
    file that the script immediately tries to read afterwards to sometimes
    be empty.  This manifests as the "Execution of NuGet not detected" error
    and I suspect is the cause of this issue on certain systems that many
    users are seeing.

--- src/functions/Run-NuGet.ps1
+++ src/functions/Run-NuGet.ps1
@@ -20,9 +20,23 @@ param(
   $logFile = Join-Path $nugetChocolateyPath 'install.log'
   $errorLogFile = Join-Path $nugetChocolateyPath 'error.log'
   Write-Debug "Calling NuGet.exe $packageArgs"
-  Start-Process $nugetExe -ArgumentList $packageArgs -NoNewWindow -Wait -RedirectStandardOutput $logFile -RedirectStandardError $errorLogFile
 
-  $nugetOutput = Get-Content $logFile -Encoding Ascii
+  $process = New-Object system.Diagnostics.Process
+  $process.StartInfo = new-object System.Diagnostics.ProcessStartInfo($nugetExe, $packageArgs)
+  $process.StartInfo.RedirectStandardOutput = $true
+  $process.StartInfo.RedirectStandardError = $true
+  $process.StartInfo.UseShellExecute = $false
+
+  $process.Start()
+  $process.WaitForExit()
+
+  $nugetOutput = $process.StandardOutput.ReadToEnd()
+  $errors = $process.StandardError.ReadToEnd()
+
+
+  $nugetOutput | Out-File $logFile
+  $errors | Out-File $errorLogFile
+
   foreach ($line in $nugetOutput) {
     if ($line -ne $null) {Write-Debug $line;}
   }
