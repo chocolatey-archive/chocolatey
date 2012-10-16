bde8c409d3ccc4123e6df10f5fb4e0b8dcb8ba5e
Rob Reynolds <ferventcoder@gmail.com>
2012-10-16 11:36:03 -0500

    updating committer notes

--- src/chocolatey.ps1
+++ src/chocolatey.ps1
@@ -17,7 +17,7 @@
 
 # chocolatey
 # Copyright (c) 2011-Present Rob Reynolds
-# Committers: Rob Reynolds, Rich Siegel, Matt Wrock
+# Committers and Contributors: Rob Reynolds, Rich Siegel, Matt Wrock, Anthony Mastrean, Alan Stevens
 # Crediting contributions by Chris Ortman, Nekresh, Staxmanade, Chrissie1, AnthonyMastrean, Rich Siegel, Matt Wrock and other contributors from the community.
 # Big thanks to Keith Dahlby for all the powershell help! 
 # Apache License, Version 2.0 - http://www.apache.org/licenses/LICENSE-2.0
