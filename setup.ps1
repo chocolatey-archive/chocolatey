71c02892a2957a010dc18601c82ba1cdad8260f2
Matt Wrock <matt@mattwrock.com>
2012-10-30 07:48:27 -0700

    get pester from myget until official feed is updated

--- setup.ps1
+++ setup.ps1
@@ -5,4 +5,4 @@ if(-not $env:ChocolateyInstall -or -not (Test-Path "$env:ChocolateyInstall")){
 
 # install nuget if it is missing
 cinstm nuget.commandline
-cinstm pester
+cinstm pester -source http://www.myget.org/F/pester/
