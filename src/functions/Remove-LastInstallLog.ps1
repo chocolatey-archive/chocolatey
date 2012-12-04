63238214d029cf78991d4acae3dd2e3e3f9764d3
Rob Reynolds <ferventcoder@gmail.com>
2012-12-04 17:07:28 -0600

    wait until after you determine that we are using the default before logging (last install log)

--- src/functions/Remove-LastInstallLog.ps1
+++ src/functions/Remove-LastInstallLog.ps1
@@ -2,11 +2,13 @@
 param(
   [string] $chocoInstallLog = ''
 )
-  Write-Debug "Running 'Remove-LastInstallLog' with chocoInstallLog:`'$chocoInstallLog`'";
-  
+
   if ($chocoInstallLog -eq '') {
     $chocoInstallLog = (Join-Path $nugetChocolateyPath 'chocolateyInstall.log')
   }
+  
+  Write-Debug "Running 'Remove-LastInstallLog' with chocoInstallLog:`'$chocoInstallLog`'";  
+  
 	try {
     if ([System.IO.File]::Exists($chocoInstallLog)) {[System.IO.File]::Delete($chocoInstallLog)}
   } catch {
