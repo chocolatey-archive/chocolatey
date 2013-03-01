be878a005fc882da0553f5d7a2cc2853cdf4ca22
Rob Reynolds <ferventcoder@gmail.com>
2013-03-01 16:39:44 -0600

    Adding Bash Compliant items

--- src/functions/Generate-BinFile.ps1
+++ src/functions/Generate-BinFile.ps1
@@ -7,16 +7,27 @@ param(
   Write-Debug "Running 'Generate-BinFile' for $name with path:`'$path`'";
 
   $packageBatchFileName = Join-Path $nugetExePath "$name.bat"
+  $packageBashFileName = Join-Path $nugetExePath "$name"
   $path = $path.ToLower().Replace($nugetPath.ToLower(), "%DIR%..\").Replace("\\","\")
+  $pathBash = $path.Replace("%DIR%..\","`$DIR/../").Replace("\","/")
   Write-Host "Adding $packageBatchFileName and pointing to `'$path`'." -ForegroundColor $Note
   if ($useStart) {
     Write-Host "Setting up $name as a non-command line application."  -ForegroundColor $Note
 "@echo off
 SET DIR=%~dp0%
-start """" ""$path"" %*" | Out-File $packageBatchFileName -encoding ASCII     
+start """" ""$path"" %*" | Out-File $packageBatchFileName -encoding ASCII    
+
+"#!/bin/sh
+DIR=`${0%/*}
+""$pathBash"" `$* & disown" | Out-File $packageBashFileName -encoding ASCII 
+ 
   } else {
 "@echo off
 SET DIR=%~dp0%
-""$path"" %*" | Out-File $packageBatchFileName -encoding ASCII     
+""$path"" %*" | Out-File $packageBatchFileName -encoding ASCII
+  
+"#!/bin/sh
+DIR=`${0%/*}
+""$pathBash"" `$*" | Out-File $packageBashFileName -encoding ASCII 
   }
 }
