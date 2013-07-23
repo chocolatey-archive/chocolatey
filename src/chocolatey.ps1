cb2715571ff645f704860888dc7d27c9eaa7ae41
Rob Reynolds <ferventcoder@gmail.com>
2013-07-23 11:45:01 -0500

    v0.9.8.21-alpha3

--- src/chocolatey.ps1
+++ src/chocolatey.ps1
@@ -41,7 +41,7 @@ $currentThread.CurrentCulture = $culture;
 $currentThread.CurrentUICulture = $culture;
 
 #Let's get Chocolatey!
-$chocVer = '0.9.8.21-alpha2'
+$chocVer = '0.9.8.21-alpha3'
 $nugetChocolateyPath = (Split-Path -parent $MyInvocation.MyCommand.Definition)
 $nugetPath = (Split-Path -Parent $nugetChocolateyPath)
 $nugetExePath = Join-Path $nuGetPath 'bin'
