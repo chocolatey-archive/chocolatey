c9a6508a73d40a39b754782279b824a8b61e976b
Rob Reynolds <ferventcoder@gmail.com>
2013-08-21 16:41:31 -0500

    spacing

--- src/functions/Get-ConfigValue.ps1
+++ src/functions/Get-ConfigValue.ps1
@@ -4,21 +4,21 @@ param(
   [ValidateNotNullOrEmpty()]
   [string] $configValue
 )
-	Write-Debug "Running 'Get-ConfigValue' with configValue:`'$configValue`'";
+  Write-Debug "Running 'Get-ConfigValue' with configValue:`'$configValue`'";
 
-    $returnValue = Get-UserConfigValue $configValue
-    Write-Debug "After checking the user config the value of `'$configValue`' is `'$returnValue`'"
-	
-    if ($returnValue -eq $null -or $returnValue -eq '') {
-        Write-Debug "Value not found in the user config file - checking the global config"
-		
-    	$returnValue = Get-GlobalConfigValue $configValue
-        Write-Debug "After checking the global config the value of `'$configValue`' is `'$returnValue`'"
-    }
+  $returnValue = Get-UserConfigValue $configValue
+  Write-Debug "After checking the user config the value of `'$configValue`' is `'$returnValue`'"
 
-    if ($returnValue -eq $null) {
-        Write-Error "A configuration value for $configValue was not found"
-    }
+  if ($returnValue -eq $null -or $returnValue -eq '') {
+    Write-Debug "Value not found in the user config file - checking the global config"
 
-    $returnValue
+    $returnValue = Get-GlobalConfigValue $configValue
+    Write-Debug "After checking the global config the value of `'$configValue`' is `'$returnValue`'"
+  }
+
+  if ($returnValue -eq $null) {
+      Write-Error "A configuration value for $configValue was not found"
+  }
+
+  $returnValue
 }
