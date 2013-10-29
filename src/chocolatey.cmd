bb4e9e0d419686b80c1449260ee95bc16461f69a
Rob Reynolds <ferventcoder@gmail.com>
2013-10-28 22:13:22 -0500

    Ensure exit code gets passed out

--- src/chocolatey.cmd
+++ src/chocolatey.cmd
@@ -10,8 +10,12 @@ if '%1'=='help' goto usage
 
 @PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "[System.Threading.Thread]::CurrentThread.CurrentCulture = ''; [System.Threading.Thread]::CurrentThread.CurrentUICulture = '';& '%DIR%chocolatey.ps1' %* ;"exit $LastExitCode""
 SET ErrLvl=%ERRORLEVEL%
-"%DIR%RefreshEnv.cmd"
+call "%DIR%RefreshEnv.cmd"
+goto :exit
+
+:exit
 exit /b %ErrLvl%
+
 goto :eof
 :usage
 
