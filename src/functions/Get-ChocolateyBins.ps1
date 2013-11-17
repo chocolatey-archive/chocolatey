4cbab6370d5c2c76dd58c11618898ee24953f88f
Rob Reynolds <ferventcoder@gmail.com>
2013-11-17 10:11:17 -0600

    spaces

--- src/functions/Get-ChocolateyBins.ps1
+++ src/functions/Get-ChocolateyBins.ps1
@@ -5,7 +5,7 @@ param(
 )
   Write-Debug "Running 'Get-ChocolateyBins' for $packageFolder";
 
-  if ($packageFolder -notlike '') { 
+  if ($packageFolder -notlike '') {
 
     Write-Debug "  __ Executable Links (*.exe) __"
 @"
@@ -37,10 +37,10 @@ Adding batch files for any executables found to a location on PATH. In other wor
     catch {
       #Write-Host 'There are no executables (that are not ignored) in the package.'
     }
-    
+
     if (!($batchCreated)) {
-      Write-Debug 'There are no executables (that are not ignored) in the package.' 
+      Write-Debug 'There are no executables (that are not ignored) in the package.'
     }
-    
+
   }
 }
