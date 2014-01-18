f9df8798a5d6068eaa874eae0e374d07cc0e0996
Rob Reynolds <ferventcoder@gmail.com>
2014-01-18 14:28:05 -0600

    (maint) updating string in setup for connection to chocolatey.org
     Instead of bit.ly

--- setup.ps1
+++ setup.ps1
@@ -1,6 +1,6 @@
 ### install chocolatey ###
 if(-not $env:ChocolateyInstall -or -not (Test-Path "$env:ChocolateyInstall")){
-    iex ((new-object net.webclient).DownloadString("http://bit.ly/psChocInstall"))
+    iex ((new-object net.webclient).DownloadString("https://chocolatey.org/install.ps1"))
 }
 
 # install nuget if it is missing
