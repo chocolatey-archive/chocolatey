1d1d9b91dbab1b9c5fe6a439bccfba937523033b
Rob Reynolds <ferventcoder@gmail.com>
2014-02-28 14:45:53 -0600

    (GH-427) Checksum downloaded files
    This provides the actual checks to determine if a checksum matches the value
    that is passed to it. This function does nothing if no checksum is included in
    the package.

--- src/helpers/functions/Get-CheckSumValid.ps1
+++ src/helpers/functions/Get-CheckSumValid.ps1
@@ -4,15 +4,26 @@ param(
   [string] $checkSum = ''
 )
   Write-Debug "Running 'Get-CheckSumValid' with file:`'$file`', checkSum: `'$checkSum`'";
-
-
   if ($checkSum -eq '' -or $checkSum -eq $null) { return }
 
-  #todo Get file's actual checksum
-
-  $fileCheckSumActual = $checkSum
-
-  if ($fileCheckSumActual -ne $checkSum) {
-    throw "CheckSum for `'$file'` did not meet `'$checkSum`'. Actual value was `'$fileCheckSumActual`'"
+  # On first install, env:ChocolateyInstall might be null still - join-path has issues
+  $md5 = Join-Path "$env:SystemDrive" 'chocolatey\chocolateyinstall\tools\md5.exe'
+  if ($env:ChocolateyInstall){
+    $md5 = Join-Path "$env:ChocolateyInstall" 'chocolateyinstall\tools\md5.exe'
   }
+
+  Write-Debug "Calling command [`'$md5`' -c$checkSum `"$file`"] to retrieve checksum"
+  $process = Start-Process "$md5" -ArgumentList " -c$checkSum `"$file`"" -Wait -WindowStyle Hidden -PassThru
+  #-OutVariable md5Output
+
+  Write-Debug "`'$md5`' exited with $($process.ExitCode)"
+
+  if ($process.ExitCode -ne 0) {
+    throw "CheckSum for `'$file'` did not meet `'$checkSum`'."
+  }
+
+  #$fileCheckSumActual = $md5Output.Split(' ')[0]
+  # if ($fileCheckSumActual -ne $checkSum) {
+  #   throw "CheckSum for `'$file'` did not meet `'$checkSum`'."
+  # }
 }
