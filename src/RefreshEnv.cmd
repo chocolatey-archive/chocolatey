c173255b09ee0c9b1181ec63e063ca64c31eba66
Rob Reynolds <ferventcoder@gmail.com>
2014-03-27 00:20:04 -0500

    GH-440 Remove annoying "Reading environment variable" messages

--- src/RefreshEnv.cmd
+++ src/RefreshEnv.cmd
@@ -10,7 +10,7 @@
 ::
 
 :: A strange trick to print without newline
-echo | set /p dummy="Reading environment variables from registry. Please wait... "
+:: echo | set /p dummy="Reading environment variables from registry. Please wait... "
 goto main
 
 :: Set one environment variable from registry key
@@ -57,8 +57,8 @@ goto main
     call "%TEMP%\_env.bat"
 
     :: Finish
-    echo | set /p dummy="Done"
-    echo .
-    
+  ::  echo | set /p dummy="Done"
+    ::echo .
+
 
 exit /b %ERRORLEVEL%
