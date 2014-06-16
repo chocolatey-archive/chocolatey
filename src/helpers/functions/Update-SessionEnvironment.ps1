c5dd82b731e1549033d3d6f945b657c686b659da
Rob Reynolds <ferventcoder@gmail.com>
2014-06-16 16:36:06 -0500

    (maint) formatting

--- src/helpers/functions/Update-SessionEnvironment.ps1
+++ src/helpers/functions/Update-SessionEnvironment.ps1
@@ -1,20 +1,20 @@
 ﻿function Update-SessionEnvironment {
 <#
 .SYNOPSIS
-Updates the environment variables of the current powershell session with 
-any environment variable changes that may have occured during a chocolatey 
+Updates the environment variables of the current powershell session with
+any environment variable changes that may have occured during a chocolatey
 package install.
 
 .DESCRIPTION
-When chocolatey installs a package, the package author may ad or change 
-certain environment variables that will affect how the application runs 
-or how it is accessed. Often, these changes are not visible to the current 
-powershell session. This means the user needs to open a new powershell 
-session before these settings take effect which can render the installed 
+When chocolatey installs a package, the package author may add or change
+certain environment variables that will affect how the application runs
+or how it is accessed. Often, these changes are not visible to the current
+powershell session. This means the user needs to open a new powershell
+session before these settings take effect which can render the installed
 application unfunctional until that time.
 
-Use the Update-SessionEnvironment command to refresh the current 
-powershell session with all environment settings possibly performed by 
+Use the Update-SessionEnvironment command to refresh the current
+powershell session with all environment settings possibly performed by
 chocolatey package installs.
 
 #>
