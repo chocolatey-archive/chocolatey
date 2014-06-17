39b52790d11ec1c6bbd654a83339acf75cb6cf41
Rob Reynolds <ferventcoder@gmail.com>
2014-06-17 01:05:52 -0500

    (maint) formatting

--- src/functions/Chocolatey-WindowsFeatures.ps1
+++ src/functions/Chocolatey-WindowsFeatures.ps1
@@ -1,12 +1,12 @@
 function Chocolatey-WindowsFeatures {
 param(
-  [string] $packageName 
+  [string] $packageName
 )
   Write-Debug "Running 'Chocolatey-WindowsFeatures' for $packageName";
-  
+
 
   Write-Host "Chocolatey (v$chocVer) is installing $packageName and dependencies (from Windows Features). By installing you accept the license for $packageName and each dependency you are installing." -ForegroundColor $RunNote -BackgroundColor Black
-  
+
   $chocoInstallLog = Join-Path $nugetChocolateyPath 'chocolateyWindowsFeaturesInstall.log';
   Append-Log $chocoInstallLog
 
@@ -25,13 +25,13 @@ if(`$dismInfo -contains 'State : Enable Pending') {return}
 "@
 
   $osVersion = (Get-WmiObject -class Win32_OperatingSystem).Version
- 
+
   $packageArgs = "/c $dism /Online /NoRestart /Enable-Feature"
   if($osVersion -ge 6.2) {
     $packageArgs += " /all"
   }
   $packageArgs += " /FeatureName:$packageName"
-  
+
   Write-Host "Opening minimized PowerShell window and calling `'cmd.exe $packageArgs`'. If progress is taking a long time, please check that window. It also may not be 100% silent..." -ForegroundColor $Warning -BackgroundColor Black
   $statements = $checkStatement + "`ncmd.exe $packageArgs | Tee-Object -FilePath `'$chocoInstallLog`';"
   Start-ChocolateyProcessAsAdmin "$statements" -minimized -nosleep -validExitCodes @(0,1)
